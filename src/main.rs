a390454bdd18ce40bb4e9bf0ed8441e0421775fe
uru <mail@nyaa.gg>
2021-05-13 15:42:06 +0900

    added check bot using at other channel

--- src/main.rs
+++ src/main.rs
@@ -184,6 +184,24 @@ async fn join(ctx: &Context, msg: &Message) -> CommandResult {
         .expect("Songbird Voice client placed in at initialisation.")
         .clone();
 
+    // check bot currently using at other channel
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let mut handler = handler_lock.lock().await;
+
+        let queue = handler.queue();
+        let current = queue.current();
+
+        if current.is_some() {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, &format!("The bot is currently playing at {}", connect_to.mention()))
+                    .await,
+            );
+
+            return Ok(());
+        }
+    }
+
     let (handle_lock, success) = manager.join(guild_id, connect_to).await;
 
     if let Ok(_channel) = success {
