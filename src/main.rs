ff98c80b26280d3ca5abf723e7f235c2d158b8d4
uru <mail@nyaa.gg>
2021-06-15 08:15:52 +0900

    when only bot in vc, leave cmd can be used

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,11 @@ async fn before(ctx: &Context, msg: &Message, command_name: &str) -> bool {
                     )
                     .await,
                 );
+            },
+            "leave" => {
+                if guild.voice_states.len() == 1 {
+                    return true;
+                }
             }
             _ => {}
         }
